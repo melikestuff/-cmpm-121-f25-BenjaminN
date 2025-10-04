fd391a6a6a6b425c1ed0dfe859106fef1704bb77
melikestuff <149332214+melikestuff@users.noreply.github.com>
2025-10-04 11:44:06 +0000

    I did the thing

--- src/main.ts
+++ src/main.ts
@@ -22,5 +22,7 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
+  counter++;
+  counterElement.innerText = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
